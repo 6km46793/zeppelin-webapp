e4a7508b53a3933c05bf8e5489cb75a777d74eab
刘志付 <zhifu@liuzhifudeMacBook-Pro.local>
2017-08-11 14:07:24 +0800

    测试脚本

--- test_shell/form_python.py
+++ test_shell/form_python.py
@@ -2,8 +2,8 @@
 
 # print('hello zeppelin')
 
-z.input('input form')
+print(z.input('input form'))
 
-z.select('select form', [(str(item), 'app' + str(item)) for item in range(1,11)], '2')
+print(z.select('select form', [(str(item), 'app' + str(item)) for item in range(1,11)], '2'))
 
-z.checkbox('checkbox form', [(str(item), 'app' + str(item)) for item in range(1,11)], [str(i) for i in range(1,5)])
+print(z.checkbox('checkbox form', [(str(item), 'app' + str(item)) for item in range(1,11)], [str(i) for i in range(1,5)]))
