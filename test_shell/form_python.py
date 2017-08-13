d8b7699cf168c637ac7c9124ab027d40f7658e08
刘志付 <zhifu@liuzhifudeMacBook-Pro.local>
2017-08-14 00:23:53 +0800

    添加 bootstrap datetimepicker 依赖

--- test_shell/form_python.py
+++ test_shell/form_python.py
@@ -4,10 +4,10 @@ print(z.input('input_form'))
 
 print(z.select('select_form', [(str(item), 'select_form' + str(item)) for item in range(1,11)], '2'))
 
-checkboxList = [(str(item), 'checkbox' + str(item)) for item in range(1,11)]
-checkboxList.insert(0, ('all','全部'))
-print(z.checkbox('checkbox', checkboxList, [str(i) for i in range(1,5)]))
+# checkboxList = [(str(item), 'checkbox' + str(item)) for item in range(1,11)]
+# checkboxList.insert(0, ('all','全部'))
+# print(z.checkbox('checkbox', checkboxList, [str(i) for i in range(1,5)]))
 
-checkboxDorpdownList =  [(str(item), 'checkbox' + str(item)) for item in range(1,11)]
-checkboxDorpdownList.insert(0, ('all','全部'))
-print(z.checkbox('checkbox_dropdown',checkboxDorpdownList, [str(i) for i in range(1,5)]))
+# checkboxDorpdownList =  [(str(item), 'checkbox' + str(item)) for item in range(1,11)]
+# checkboxDorpdownList.insert(0, ('all','全部'))
+# print(z.checkbox('checkbox_dropdown',checkboxDorpdownList, [str(i) for i in range(1,5)]))
