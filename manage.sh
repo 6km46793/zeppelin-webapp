fdb0b312c43ac8c1e02797b84c49347af9f1d940
zhifu <zhifu>
2017-09-23 19:07:46 +0800

    重启的时候，webapps 解压慢，导致后面解压修改的 zeppelin-webapp 项目冲突，先sleep 1

--- manage.sh
+++ manage.sh
@@ -23,6 +23,7 @@ deploy()
 {
     # 重启服务
     sudo ../zeppelin-0.7.2-bin-all/bin/zeppelin-daemon.sh restart
+    sleep 1
     targetPath="../zeppelin-0.7.2-bin-all/webapps/webapp"
     warFileName="zeppelin-web-0.7.2.war"
     if [ ! -d ${targetPath} ]; then
